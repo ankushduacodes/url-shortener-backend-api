66ccc425336e7f75a21257558e034afeaf47ef89
ankushduacodes <ankushduacodes@gmail.com>
2021-05-24 00:45:52 +0530

    comments on what needs to be done

--- src/app.js
+++ src/app.js
@@ -8,8 +8,17 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.post('/:id', (req, res) => {
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+
+// eslint-disable-next-line
+app.post('/', (req, res) => {
+  // code for generating unique id and save it to the db and send response url json
   res.end(JSON.stringify({ message: 'Hello' }));
+});
+
+app.get('/:id', (req, res) => {
+  // code for checking if the id in the db exists, if it does then redirect with 301 to saved url
+  res.redirect('', 301);
 });
 
 app.listen(port, () => {
